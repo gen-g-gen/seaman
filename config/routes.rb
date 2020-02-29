90f52de14d87970686be93e55f9f041514db998e
gen-g-gen <59825257+gen-g-gen@users.noreply.github.com>
2020-03-01 08:56:15 +0900

    dvise　comp...
    ・deviseの導入
    ・facebook認証、google認証の完了
    　⇨linkとんでもidが無効となってる...
        ⇨あとで確認。まずは、それ以外を進める。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+  
   root 'messages#index'
-  resources :messages, only: [:index]
+  resources :messages, only: [:index, :new]
+  resources :users, only: [:index, :new]
 
 end

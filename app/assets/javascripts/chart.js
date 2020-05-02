a9430c609e6d9757b2b9df8d164afaa8e7f21143
gen-g-gen <59825257+gen-g-gen@users.noreply.github.com>
2020-05-02 17:52:26 +0900

    導入完了
    検索結果をチャートで表示できる！
    表示は２個まで。
    
    今はmessage#indexでのみ動くが、それ以外でも動かせるようにする

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -4,12 +4,12 @@
     //           chart.js
     // ----------------------------
 
-    var drawGraph = function(data){
+    var drawGraph = function(data, labels){
         var ctx = document.getElementById('chart').getContext('2d');
       // データ1
-      var pointName = gon.pointname
+      
         var data1 = {
-            label: pointName,
+            label: labels[0],
             data:data[1],
             backgroundColor: "rgba(250, 50, 50, 0.3)",
             borderColor: "rgba(200, 50, 50, 0.3)",
@@ -18,7 +18,7 @@
         };
       // データ2
         var data2 = {
-            label: "テスト",  //pointName2として、変数の代入
+            label: labels[1], 
             data:data[2],
             backgroundColor: "rgba(50, 50, 250, 0.3)",
             borderColor: "rgba(50, 50, 200, 0.3)",
@@ -70,25 +70,30 @@
 
     };
 
-    // ラベルの設定
+    // データの設定
     var data1 =  gon.array
+    var pointName = gon.pointname
+    var newPointName = "検索してください"
+   
     var data = [['波', '風', '人口', 'セット', '期待値'],
                 data1,
-                [2,2,2,2,2]]  //data2には、aja使ってコントローラから変数の代入を行う
-    
-    drawGraph(data);
-
+                [0,0,0,0,0]]  
+    var labels =  [ pointName , newPointName ]
+    drawGraph(data, labels);
     // ----------------------------
     //           検索機能
     // ----------------------------
 
-    function addData(chart, newlabel, newdata) {
-      console.log(data2)
-      data2.label.push(newlabel);
-      data.datasets.pop();
-      data.datasets[3].push(newdata);
-      chart.update();
-  }
+    function addData(newdata,newlabel) {
+      data.pop();
+      labels.pop();
+      data.push(newdata);
+      labels.push(newlabel);
+      
+      console.log(data)
+      console.log(labels)
+      drawGraph(data, labels);
+    }
   
     
 
@@ -167,13 +172,10 @@
         })
         .done(function(points) {
           if (points.length !== 0) {
-            var pointData = points.data
-
             var newlabel = pointName
-            var newdata = pointData
+            var newdata = points.data
 
-            addData(chart, newlabel, newdata);
-            // drawGraph(data);
+            addData(newdata, newlabel);
 
          } else {
           console.log("pointsない");
@@ -184,6 +186,6 @@
           alert("point検索に失敗しました");
         });
     });
-    
+
   });
    

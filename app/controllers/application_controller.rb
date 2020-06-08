66ccd49e475b5a065aab0c1241954076fbffae7b
gen-g-gen <59825257+gen-g-gen@users.noreply.github.com>
2020-06-08 15:20:12 +0900

    リファクタリング_Controller
    paramsの修正
    chartメソッドの省略

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :point
+
 
   def after_sign_in_path_for(resource)
     area_point_messages_path(current_user.area_id, current_user.homepoint_id)
